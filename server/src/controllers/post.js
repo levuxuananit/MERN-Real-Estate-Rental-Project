dac0b6412f80f4c581a062e635f137c96dea3b71
jingjie <vosonhung03@gmail.com>
2024-11-14 23:52:18 +0700

    create api for find post by id

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -1,39 +1,53 @@
-import * as postService from '../services/post.js'
+import * as postService from "../services/post.js";
 
 export const getPosts = async (req, res) => {
-    try {
-        const response = await postService.getPostsService()
-        return res.status(200).json(response)
-
-    } catch (error) {
-        return res.status(500).json({
-            err: -1,
-            msg: 'Failed at post controller: ' + error
-        })
-    }
-}
+  try {
+    const response = await postService.getPostsService();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Failed at post controller: " + error,
+    });
+  }
+};
 export const getPostsLimit = async (req, res) => {
-    const { page, priceNumber, areaNumber, ...query } = req.query
-    try {
-        const response = await postService.getPostsLimitService(page, query, { priceNumber, areaNumber })
-        return res.status(200).json(response)
-
-    } catch (error) {
-        return res.status(500).json({
-            err: -1,
-            msg: 'Failed at post controller: ' + error
-        })
-    }
-}
+  const { page, priceNumber, areaNumber, ...query } = req.query;
+  try {
+    const response = await postService.getPostsLimitService(page, query, {
+      priceNumber,
+      areaNumber,
+    });
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Failed at post controller: " + error,
+    });
+  }
+};
 export const getNewPosts = async (req, res) => {
-    try {
-        const response = await postService.getNewPostService()
-        return res.status(200).json(response)
+  try {
+    const response = await postService.getNewPostService();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Failed at post controller: " + error,
+    });
+  }
+};
 
-    } catch (error) {
-        return res.status(500).json({
-            err: -1,
-            msg: 'Failed at post controller: ' + error
-        })
-    }
-}
+export const findPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await postService.findPostByIdService(id);
+
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Failed at post controller: " + error,
+    });
+  }
+};
--- server/src/services/post.js
+++ server/src/services/post.js
@@ -1,87 +1,122 @@
-import Post from '../models/post.js';
-import Image from '../models/image.js';
-import Attribute from '../models/attribute.js';
-import User from '../models/user.js';
-import dotenv from 'dotenv';
+import Post from "../models/post.js";
+import Image from "../models/image.js";
+import Attribute from "../models/attribute.js";
+import User from "../models/user.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // GET ALL POSTS
-export const getPostsService = () => new Promise(async (resolve, reject) => {
+export const getPostsService = () =>
+  new Promise(async (resolve, reject) => {
     try {
-        const response = await Post.find({})
-            // .populate({ path: 'images', select: 'image' })
-            .populate({
-                path: 'attributesId',
-                foreignField: 'id', //tìm theo id
-                select: 'id price acreage published hashtag', // Select the relevant fields
-                strictPopulate: false // Allow population with UUID (string) references
-            });
-        // .populate({ path: 'user', select: 'name zalo phone' })
-        // .select('id title star address description')
-        // .lean(); // `.lean()` returns plain JavaScript objects
-
-        resolve({
-            err: response ? 0 : 1,
-            msg: response ? 'OK' : 'Getting posts failed.',
-            response
+      const response = await Post.find({})
+        // .populate({ path: 'images', select: 'image' })
+        .populate({
+          path: "attributesId",
+          foreignField: "id", //tìm theo id
+          select: "id price acreage published hashtag", // Select the relevant fields
+          strictPopulate: false, // Allow population with UUID (string) references
         });
+      // .populate({ path: 'user', select: 'name zalo phone' })
+      // .select('id title star address description')
+      // .lean(); // `.lean()` returns plain JavaScript objects
+
+      resolve({
+        err: response ? 0 : 1,
+        msg: response ? "OK" : "Getting posts failed.",
+        response,
+      });
     } catch (error) {
-        reject(error);
+      reject(error);
     }
-});
+  });
 
 // GET POSTS WITH LIMIT AND FILTER
-export const getPostsLimitService = (page, query, { priceNumber, areaNumber }) =>
-    new Promise(async (resolve, reject) => {
-        try {
-            const limit = +process.env.LIMIT;
-            const offset = (!page || +page <= 1) ? 0 : (page - 1) * limit;
+export const getPostsLimitService = (
+  page,
+  query,
+  { priceNumber, areaNumber }
+) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const limit = +process.env.LIMIT;
+      const offset = !page || +page <= 1 ? 0 : (page - 1) * limit;
 
-            // Constructing Mongoose query filter
-            const filter = { ...query };
-            if (priceNumber) filter.priceNumber = { $gte: priceNumber[0], $lte: priceNumber[1] };
-            if (areaNumber) filter.areaNumber = { $gte: areaNumber[0], $lte: areaNumber[1] };
+      // Constructing Mongoose query filter
+      const filter = { ...query };
+      if (priceNumber)
+        filter.priceNumber = { $gte: priceNumber[0], $lte: priceNumber[1] };
+      if (areaNumber)
+        filter.areaNumber = { $gte: areaNumber[0], $lte: areaNumber[1] };
 
-            const response = await Post.find(filter)
-                .skip(offset)
-                .limit(limit)
-                .populate({ path: 'images', select: 'image' })
-                .populate({ path: 'attributes', select: 'price acreage published hashtag' })
-                .populate({ path: 'user', select: 'name zalo phone' })
-                .select('id title star address description')
-                .lean();
+      const response = await Post.find(filter)
+        .skip(offset)
+        .limit(limit)
+        .populate({ path: "images", select: "image" })
+        .populate({
+          path: "attributes",
+          select: "price acreage published hashtag",
+        })
+        .populate({ path: "user", select: "name zalo phone" })
+        .select("id title star address description")
+        .lean();
 
-            const totalCount = await Post.countDocuments(filter);
+      const totalCount = await Post.countDocuments(filter);
 
-            resolve({
-                err: response.length ? 0 : 1,
-                msg: response.length ? 'OK' : 'Getting posts failed.',
-                response,
-                totalCount
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
+      resolve({
+        err: response.length ? 0 : 1,
+        msg: response.length ? "OK" : "Getting posts failed.",
+        response,
+        totalCount,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 
 // GET NEW POSTS
-export const getNewPostService = () => new Promise(async (resolve, reject) => {
+export const getNewPostService = () =>
+  new Promise(async (resolve, reject) => {
     try {
-        const response = await Post.find({})
-            .sort({ createdAt: -1 })
-            .limit(+process.env.LIMIT)
-            .populate({ path: 'images', select: 'image' })
-            .populate({ path: 'attributes', select: 'price acreage published hashtag' })
-            .select('id title star createdAt')
-            .lean();
+      const response = await Post.find({})
+        .sort({ createdAt: -1 })
+        .limit(+process.env.LIMIT)
+        .populate({ path: "images", select: "image" })
+        .populate({
+          path: "attributes",
+          select: "price acreage published hashtag",
+        })
+        .select("id title star createdAt")
+        .lean();
 
-        resolve({
-            err: response ? 0 : 1,
-            msg: response ? 'OK' : 'Getting new posts failed.',
-            response
-        });
+      resolve({
+        err: response ? 0 : 1,
+        msg: response ? "OK" : "Getting new posts failed.",
+        response,
+      });
     } catch (error) {
-        reject(error);
+      reject(error);
     }
-});
+  });
+export const findPostByIdService = (postId) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await Post.findById(postId)
+        .populate({ path: "images", select: "image" })
+        .populate({
+          path: "attributes",
+          select: "price acreage published hashtag",
+        })
+        .select("id title star createdAt")
+        .lean();
+
+      resolve({
+        err: response ? 0 : 1,
+        msg: response ? "OK" : "Post not found.",
+        response,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
